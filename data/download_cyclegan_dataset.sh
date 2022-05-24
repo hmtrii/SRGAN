1940d26c56fb0c6e84da947e54090235c09e954d
trihuynh <hminhtri19@gmail.com>
2022-05-25 00:04:09 +0700

    add generate lr cyclegan dataset

--- data/download_cyclegan_dataset.sh
+++ data/download_cyclegan_dataset.sh
@@ -8,10 +8,10 @@ if [[ $FILE != "ae_photos" && $FILE != "apple2orange" && $FILE != "summer2winter
 fi
 
 URL=https://people.eecs.berkeley.edu/~taesung_park/CycleGAN/datasets/$FILE.zip
-ZIP_FILE=./data/$FILE.zip
-TARGET_DIR=./data/$FILE
+ZIP_FILE=./data/cyclegan/$FILE.zip
+TARGET_DIR=./data/cyclegan/$FILE
 wget -N $URL -O $ZIP_FILE
-unzip $ZIP_FILE -d ./data
+unzip $ZIP_FILE -d ./data/cyclegan
 rm $ZIP_FILE
 
 # Adapt to project expected directory heriarchy
