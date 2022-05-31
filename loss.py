7b0011303cff2fa03dd8b0e1ca4df3211bb7e89f
trihuynh <hminhtri19@gmail.com>
2022-05-31 21:22:39 +0700

    update

--- loss.py
+++ loss.py
@@ -1,10 +1,48 @@
+import torch
 from torch import nn
-from torchvision.models.vgg import vgg19  
+from torch.nn import functional as F
+from torchvision.models.vgg import vgg19
+from torchvision.models.feature_extraction import create_feature_extractor
+from torchvision import transforms as T
+
 
 class ContentLoss(nn.Module):
-    def __init__(self) -> None:
+    def __init__(self, ith_pool, jth_cnv) -> None:
         super(ContentLoss).__init__()
-        feature_extractor = vgg19
+        self.model = vgg19(pretrained=True)
+        self.ith_pool = ith_pool
+        self.jth_conv = jth_cnv
+        self.feature_extractor = self.create_feature_extractor_()
+        self.id_layer = self.create_id_layer()
+
+    def create_id_layer(self):
+        if self.ith_pool <= 2:
+            id = 5 * self.ith_pool - self.jth_conv * 2
+        else:
+            id = (10 + 9 * (self.ith_pool - 2)) - self.jth_conv * 2
+        return f'feature.{id}'
     
-    def forward(self, hr_images, lr_images):
-        return
+    def create_feature_extractor_(self):
+        feature_extractor = create_feature_extractor(self.model, [self.id_layer])
+        feature_extractor.eval()
+        for params in feature_extractor.parameters():
+            params.requires_grad = False
+        return feature_extractor
+
+    def forward(self, hr_tensor: torch.tensor, lr_tensor: torch.tensor):
+        feature_hr = self.feature_extractor(hr_tensor)[self.id_layer]
+        feature_lr = self.feature_extractor(lr_tensor)[self.id_layer]
+        return F.mse_loss(feature_hr, feature_lr)
+
+class AversarialLoss(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+        self.criterion = nn.BCELoss()
+
+    def forward(self, prob_sr, prob_hr, batch_size, device):
+        sr_labels = torch.full([batch_size, 0], 0.0, dtype=prob_sr.dtype, device=device)
+        hr_labels = torch.full([batch_size, 0], 1.0, dtype=prob_hr.dtype, device=device)
+        labels = torch.concat([sr_labels, hr_labels], dim=0)
+        preds = torch.concat([prob_sr, prob_hr])
+        loss = self.criterion(preds, labels)
+        return loss, sr_labels
--- train.py
+++ train.py
@@ -4,10 +4,12 @@ import tqdm
 import os
 
 import torch
+from torch import nn
 from torch.utils.data import DataLoader
 
 from datasets import TrainSetCycleGan
 from model import Generator, Discriminator
+from loss import ContentLoss, AversarialLoss
 
 
 def load_configs(config_path):
@@ -29,6 +31,8 @@ if __name__ == '__main__':
     batch_size = configs['batch_size']
     num_workers = configs['num_workers']
     epochs = configs['epochs']
+    ith_pool = configs['ith_pool']
+    jth_cnv = configs['jth_cnv']
 
     train_set = TrainSetCycleGan(root_dir, train_subset)
     val_set = TrainSetCycleGan(root_dir, val_subset)
@@ -38,15 +42,28 @@ if __name__ == '__main__':
     dis_model = Discriminator()
     gen_optimizer = torch.optim.Adam(gen_model.parameters())
     dis_optimizer = torch.optim.Adam(dis_model.parameters())
-
+    gen_criterion = ContentLoss(ith_pool, jth_cnv).to(device)
+    adver_criterion = nn.BCELoss().to(device)
     train_loader = DataLoader(train_set, batch_size=batch_size)
 
     for epoch in tqdm(range(epochs)):
         gen_model.train()
         dis_model.train()
-        for hr_image, lr_image in train_loader:
+        for hr_tensor, lr_tensor in train_loader:
+            sr_tensor = gen_model(lr_tensor)
+            # Discrim network
+            dis_optimizer.zero_grad()
+            prob_sr = dis_model(sr_tensor)
+            prob_hr = dis_model(hr_tensor)
+            fake_labels = torch.full([batch_size, 0], 0.0, dtype=prob_sr.dtype, device=device)
+            real_labels = torch.full([batch_size, 0], 1.0, dtype=prob_hr.dtype, device=device)
+            dis_loss_sr = adver_criterion(prob_sr)
+            dis_loss_hr = adver_criterion(hr_tensor)
+            dis_loss = (dis_loss_sr + dis_loss_hr)
+            # Gen network
             gen_optimizer.zero_grad()
-            gen_hr_image = gen_model(lr_image)
-            loss = 
+            content_loss = gen_criterion(hr_tensor, lr_tensor)
+            
+            
 
         
